9fe48ef671322b699216f5cf87d5095a7f6adb17
probonopd <probonopd@users.noreply.github.com>
2023-08-13 14:40:31 +0200

    Remove snapd

--- build/switch.sh
+++ build/switch.sh
@@ -5,6 +5,11 @@
 wget https://gist.github.com/probonopd/2f53a49b021f270b9106d8f8bb1f199b/raw/customize-lubuntu.sh
 su lubuntu -c "/bin/sh -x ./customize-lubuntu.sh"
 rm ./customize-lubuntu.sh
+apt-get -y remove snapd || true
+rm -rf /var/lib/snapd || true
+apt-get -y install falkon
+
+############################################
 
 # Clean up system files
 apt-get clean -y
