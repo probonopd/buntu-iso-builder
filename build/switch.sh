832255d6fa766cfb6c7c90ea2e4f566bd4fbe80d
probonopd <probonopd@users.noreply.github.com>
2023-09-23 20:05:43 +0000

    libgtk-3-0

--- build/switch.sh
+++ build/switch.sh
@@ -28,8 +28,8 @@ EOF
 
 # Install software
 apt-get -y install libfuse2 falkon baloo-kf5 libqalculate22 plasma-integration libqt5multimedia5 plymouth-theme-spinner \
-python3-xattr python3-psutil libqaccessibilityclient-qt5-0 libxcb-cursor0 elementary-xfce-icon-theme libatk1.0-0 libatk-bridge2.0-0
-# libatk1.0-0 libatk-bridge2.0-0 are there so that the Ungoogled Chromium AppImage can run; could be removed otherwise
+python3-xattr python3-psutil libqaccessibilityclient-qt5-0 libxcb-cursor0 elementary-xfce-icon-theme libgtk-3-0
+# libgtk-3-0 is there so that many AppImages can run; could be removed otherwise
 
 # desktop2app
 ln -s /usr/share/applications /usr/local/share/
